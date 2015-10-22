5b19b47ff79c7d373b6810354c5de0ca94c05b44
marb <marb61a@gmail.com>
2015-10-22 08:03:59 +0000

    added movies model and controller

---
+++ controllers/movies.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Movie = require("../models/movies");
+
+module.exports = function(router){
+    var model = new Movie();
+    router.get('/', function (req, res) {
+        Movie.find({}, function(err, movies){
+        	if(err){
+        		res.send(err);
+        	} else {
+        		var model = {
+        			movies: movies
+        		}
+
+        		res.render('movies', model);
+        	}
+        });  
+    });
+};
