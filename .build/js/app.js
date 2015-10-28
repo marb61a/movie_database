0341c56704c4a086ef225f9015f1cec6bbe6fb07
marb <marb61a@gmail.com>
2015-10-28 09:18:49 +0000

    updated templates

--- .build/js/app.js
+++ .build/js/app.js
@@ -1,19 +1,21 @@
-'use strict';
-
-
-requirejs.config({
-    paths: {}
-});
-
-
-require([/* Dependencies */], function () {
-
-    var app = {
-        initialize: function () {
-            // Your code here
-        }
-    };
-
-    app.initialize();
-
-});
+$(document).ready(function() {
+    $('.delete-movie').click(function(event) {
+    $target = $(event.target);
+    $.ajax({
+      type: 'DELETE',
+      url: '/movies/delete/' + $target.attr('data-movie-id'),
+      data: {
+        _csrf: $target.attr('data-csrf')
+      },
+      success: function(response) {
+        $target.parent().parent().remove();
+        alert('Movie Removed');
+        window.location.href = '/movies';
+      },
+      error: function(error) {
+        alert(error);
+          console.log(error);
+      }
+    });
+  });
+})
